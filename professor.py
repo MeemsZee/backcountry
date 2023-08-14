3cd614216623203f9a22990678cabea283455182
MeemsZee <MeemsZee@users.noreply.github.com>
2023-08-14 16:54:18 -0600

    automated commit by check50 [check50=True]

--- professor.py
+++ professor.py
@@ -5,68 +5,76 @@ After 10 questions, program will spit out a score
 """
 import random
 
+
 def main():
+    # ask user for level of difficulty
     level = get_level()
+
+    # inialize total num  of questions, tries and score
     num_quest = 10
     tries = 3
-    # prompt user to solve questions
     score = 0
-    questions = 0
 
     for i in range(num_quest):
-
+        # generate an x and y
         x = generate_integer(level)
         y = generate_integer(level)
-        print(f"{x} + {y} = ", end="")
-        ans = int(input().strip())
+
+        # get correct answer
         correct = x + y
-        j = 1
+
+        j = 0
+
+        # give player 3 tries to get the correct answer
         while j <= tries:
+            # add a try
+            j += 1
+
+            # print question
+            print(f"{x} + {y} = ", end="")
+
+            # if answer isn't a number, raise an exception, if it is, break out of current loop
             try:
-                if ans != correct:
-                    print("EEE")
-                    if j == tries:
-                        print(f"{x} + {y} = {correct}" )
-                        score -= 1
-                        break
-                    print(f"{x} + {y} = ", end="")
-                    ans = int(input().strip())
-                    if ans == correct:
-                        break
-                    j += 1
-                else:
+                ans = int(input().strip())
+                if ans == correct:
+                    score += 1
                     break
             except ValueError:
                 pass
 
-        score += 1
-        questions +=1
-        if questions == num_quest:
-            break
+            # if answer isn't correct print EEE, if this is the 3rd try, provide the correct answer
+            print("EEE")
+            if j == tries:
+                print(f"Correct answer: {x} + {y} = {correct}")
+                break
 
+    # print total score
     print(f"Score: {score}")
+
 
 def get_level():
     # Get an input from the user. Only n of 1, 2, or 3 are accepted
     while True:
+        num_list = [1, 2, 3]
         try:
             n = int(input("Level: ").strip())
-            if n < 1 or n > 3:
-                pass
-            else:
-                return n
         except ValueError:
             pass
+
+        if n in num_list:
+            return n
+
 
 def generate_integer(level):
     # depending on the level user inputted, generate an integer
     match level:
         case 1:
-            return(random.randint(0, 9))
+            return random.randint(0, 9)
         case 2:
-            return(random.randint(10, 99))
+            return random.randint(10, 99)
         case 3:
-            return(random.randint(100, 999))
+            return random.randint(100, 999)
+
 
 if __name__ == "__main__":
     main()
