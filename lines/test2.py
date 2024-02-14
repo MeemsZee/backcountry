a24a1778593a884cdf169db757757c60fd218944
MeemsZee <91973217+MeemsZee@users.noreply.github.com>
2024-02-14 14:36:28 -0700

    Delete lines directory

--- lines/test2.py
+++
@@ -1,4 +0,0 @@
-animals = ["cat", "dog", "turtle", "fish", "horse"]
-
-for animal in animals:
-    print(f"Animal: {animal}")
--- lines/test3.py
+++
@@ -1,7 +0,0 @@
-import json
-import requests
-import sys
-
-# if there isn't a number provided in the CLI, exit 
-if len(sys.argv) != 2:
-    sys.exit("Missing command-line argument")
